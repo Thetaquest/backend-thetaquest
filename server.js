74064210434768bd9031c196985747b6e0128ca9
Jazthedev <joelpepa2@gmail.com>
2023-05-15 16:07:00 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
-const router_quiz = require('./routers/router_quiz');
+const mongoose = require('mongoose');
+const routerquiz = require('./routers/router_quiz');
+const routeruser = require('./routers/router_user');
+const dotenv = require('dotenv').config()
+const http = require("http");
+
+mongoose.connect(process.env.MONGODB_URI2).then(res=>{
+    console.log('database connected')
+});
+
 
 const app = express();
+
 const PORT = 3000;
-app.use('/api/quiz',router_quiz);
+//create the req.body 
+app.use(express.json());
+
+app.use('/api/quiz',routerquiz);
+app.use('/api/user',routeruser);
+
+app.all("*",(req,res,next)=>{
+    return res.status(200).json({
+        message: 'this route dont exists , please try again'
+    });
+})
+
 app.listen(PORT, (error) =>{
     if(!error){
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+        console.log("server is successfully running, and App is listening on port "+ PORT)
     }else{
         console.log("Error occurred, server can't start", error);
     }
